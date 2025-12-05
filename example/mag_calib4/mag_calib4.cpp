e66356eaaf615c10fddd07c01e34915eb1e989d4
yucejade <121438331@qq.com>
2025-12-05 17:38:15 +0800

    修改校准程序

--- example/mag_calib4/mag_calib4.cpp
+++ example/mag_calib4/mag_calib4.cpp
@@ -1,9 +1,9 @@
-#include "fm_device_wrapper.h"
-#include "SoftAndHardIronCalibration.h"
 #include "SixParametersCorrector.h"
-#include <rapidcsv.h>
+#include "SoftAndHardIronCalibration.h"
+#include "fm_device_wrapper.h"
 #include <algorithm>
 #include <iostream>
+#include <rapidcsv.h>
 #include <string>
 #include <vector>
 
@@ -39,12 +39,17 @@ public:
         if ( strValue.empty() )
             return defaultValue;
 
-        try {
+        try
+        {
             return std::stoi( strValue );
-        } catch ( const std::invalid_argument& ) {
+        }
+        catch ( const std::invalid_argument& )
+        {
             // 字符串不是整数（如"-i abc"），返回默认值
             return defaultValue;
-        } catch ( const std::out_of_range& ) {
+        }
+        catch ( const std::out_of_range& )
+        {
             // 整数超出int范围（如"-i 1234567890123"），返回默认值
             return defaultValue;
         }
@@ -90,37 +95,47 @@ int main( int argc, char* argv[] )
     {
         std::string calibration_path = parser.getOption( "-c" );
         if ( calibration_path.empty() )
-        {
             calibration_path = parser.getOption( "--calibration-path" );
-            if ( calibration_path.empty() )
-                calibration_path = "./mag_calib.csv";
-        }
+
+        std::string mag_data_path = parser.getOption( "-d" );
+        if ( mag_data_path.empty() )
+            mag_data_path = parser.getOption( "--mag-data-path" );
+
+        // calibration_path和mag_data_path只允许输入一个
+        if ((calibration_path.empty() && mag_data_path.empty()) || (!calibration_path.empty() && !mag_data_path.empty()))
+            return -1;
 
         int interval;
-        if (parser.hasOption( "-i" ))
+        if ( parser.hasOption( "-i" ) )
             interval = parser.getIntOption( "-i" );
-        else if (parser.hasOption( "--interval" ))
+        else if ( parser.hasOption( "--interval" ) )
             interval = parser.getIntOption( "--interval" );
         else
             interval = 180;  // 默认时间间隔
-        
+
         try
         {
-            fm_device_handle_t         device_handler;
-            SensorData                 sensor_data;
-            bool                       is_first = true;
-            int                        count    = 0;
-            SixParametersCorrector     loaded_corrector;
-            int                        ret = PDR_RESULT_SUCCESS;
+            fm_device_handle_t     device_handler;
+            SensorData             sensor_data;
+            bool                   is_first = true;
+            int                    count    = 0;
+            SixParametersCorrector loaded_corrector;
+            int                    ret = PDR_RESULT_SUCCESS;
 
-            if (loaded_corrector.fromFile(calibration_path)) {
-                std::cout << "成功加载校准参数！" << std::endl;
-                // 打印加载的参数（可选，用于验证）
-                std::cout << "加载的硬铁偏移（b）: " << loaded_corrector.getb().transpose() << " μT" << std::endl;
-                std::cout << "加载的软铁增益（A）: " << loaded_corrector.getA().transpose() << "（无单位）" << std::endl;
-            } else {
-                std::cerr << "加载校准参数失败！" << std::endl;
-                return 1;
+            if ( ! calibration_path.empty() )
+            {
+                if ( loaded_corrector.fromFile( calibration_path ) )
+                {
+                    std::cout << "成功加载校准参数！" << std::endl;
+                    // 打印加载的参数（可选，用于验证）
+                    std::cout << "加载的硬铁偏移（b）: " << loaded_corrector.getb().transpose() << " μT" << std::endl;
+                    std::cout << "加载的软铁增益（A）: " << loaded_corrector.getA().transpose() << "（无单位）" << std::endl;
+                }
+                else
+                {
+                    std::cerr << "加载校准参数失败！" << std::endl;
+                    return 1;
+                }
             }
 
             ret = fm_device_init( 50, &device_handler );
@@ -129,8 +144,8 @@ int main( int argc, char* argv[] )
 
             memset( &sensor_data, 0x00, sizeof( sensor_data ) );
 
-            if (interval > 0)
-                interval = (interval % 2 != 0 ? interval + 1 : interval) / 2;
+            if ( interval > 0 )
+                interval = ( interval % 2 != 0 ? interval + 1 : interval ) / 2;
             while ( interval <= 0 || ++count < interval )
             {
                 const int length = 50 * 2;
@@ -146,34 +161,40 @@ int main( int argc, char* argv[] )
                 // 标记不是第一次读取数据，即不需要再次创建缓存
                 is_first = false;
 
-                for (int i = 0; i < length; ++i)
+                if ( ! calibration_path.empty() )
                 {
-                    const double& timestamp = sensor_data.sensor_data.acc_time[i];
-                    const double& mag_x = sensor_data.sensor_data.mag_x[i];
-                    const double& mag_y = sensor_data.sensor_data.mag_y[i];
-                    const double& mag_z = sensor_data.sensor_data.mag_z[i];
-                    MagnetometerData raw_data(timestamp, mag_x, mag_y, mag_z);
-                    Vector3f raw_vec(raw_data.magneticFieldX, raw_data.magneticFieldY, raw_data.magneticFieldZ);
+                    for ( int i = 0; i < length; ++i )
+                    {
+                        const double&    timestamp = sensor_data.sensor_data.acc_time[ i ];
+                        const double&    mag_x     = sensor_data.sensor_data.mag_x[ i ];
+                        const double&    mag_y     = sensor_data.sensor_data.mag_y[ i ];
+                        const double&    mag_z     = sensor_data.sensor_data.mag_z[ i ];
+                        MagnetometerData raw_data( timestamp, mag_x, mag_y, mag_z );
+                        Vector3f         raw_vec( raw_data.magneticFieldX, raw_data.magneticFieldY, raw_data.magneticFieldZ );
 
-                    // 调用校正方法（公式：校正后 = (原始数据 - 偏移) × 增益）
-                    Vector3f corrected_vec = loaded_corrector.correct(raw_vec);
+                        // 调用校正方法（公式：校正后 = (原始数据 - 偏移) × 增益）
+                        Vector3f corrected_vec = loaded_corrector.correct( raw_vec );
 
-                    // 输出校正结果
-                    std::cout << "\n=== 数据校正示例 ===" << std::endl;
-                    std::cout << "原始数据: " << raw_vec.transpose() << " μT" << std::endl;
-                    std::cout << "校正后数据: " << corrected_vec.transpose() << ", " << corrected_vec.norm() << " μT" << std::endl;
+                        // 输出校正结果
+                        std::cout << "\n=== 数据校正示例 ===" << std::endl;
+                        std::cout << "原始数据: " << raw_vec.transpose() << " μT" << std::endl;
+                        std::cout << "校正后数据: " << corrected_vec.transpose() << ", " << corrected_vec.norm() << " μT" << std::endl;
+                    }
+                }
 
-                    // // 计算模长
-                    // double magnitude_before = std::sqrt(mag_x * mag_x + mag_y * mag_y + mag_z * mag_z);
-                    // std::cout << "校准前数据：(" << mag_x << "," << mag_y << "," << mag_z << "," << magnitude_before << ")" << std::endl;
+                if ( ! mag_data_path.empty() )
+                {
+                    // 转换为PDRData结构
+                    PDRData pdr_data;
+                    memset( &pdr_data, 0x00, sizeof( pdr_data ) );
+                    pdr_data.sensor_data = sensor_data.sensor_data;
 
-                    // calib.Calibration( mag_x, mag_y, mag_z );
-
-                    // mag_x *= sensor_data.sensor_data.mag_x[i];
-                    // mag_y *= sensor_data.sensor_data.mag_y[i];
-                    // mag_z *= sensor_data.sensor_data.mag_z[i];
-                    // double magnitude_after = std::sqrt(mag_x * mag_x + mag_y * mag_y + mag_z * mag_z);
-                    // std::cout << "校准后数据：(" << mag_x << "," << mag_y << "," << mag_z << "," << magnitude_after << ")" << std::endl;
+                    int result = fm_pdr_save_pdr_data( ( char* )mag_data_path.c_str(), &pdr_data );
+                    if ( result != 0 )
+                    {
+                        std::cerr << "Failed to save data." << std::endl;
+                        continue;
+                    }
                 }
             }
 
@@ -207,31 +228,34 @@ int main( int argc, char* argv[] )
         try
         {
             SoftAndHardIronCalibration calib;
-            rapidcsv::Document doc(mag_data_path, rapidcsv::LabelParams( 0 ));
-            std::vector<double> mag_x = doc.GetColumn< double >( "X (µT)" );  // 提取"X (µT)"列的所有数据
-            std::vector<double> mag_y = doc.GetColumn< double >( "Y (µT)" );  // 提取"Y (µT)"列的所有数据
-            std::vector<double> mag_z = doc.GetColumn< double >( "Z (µT)" );  // 提取"Z (µT)"列的所有数据
+            rapidcsv::Document         doc( mag_data_path, rapidcsv::LabelParams( 0 ) );
+            std::vector< double >      mag_x = doc.GetColumn< double >( "X (µT)" );  // 提取"X (µT)"列的所有数据
+            std::vector< double >      mag_y = doc.GetColumn< double >( "Y (µT)" );  // 提取"Y (µT)"列的所有数据
+            std::vector< double >      mag_z = doc.GetColumn< double >( "Z (µT)" );  // 提取"Z (µT)"列的所有数据
 
-            for (size_t i = 0; i < mag_x.size(); ++i)
+            for ( size_t i = 0; i < mag_x.size(); ++i )
             {
                 MagnetometerData data;
 
                 // 复制文件中的数据
-                data.magneticFieldX = mag_x[i];
-                data.magneticFieldY = mag_y[i];
-                data.magneticFieldZ = mag_z[i];
+                data.magneticFieldX = mag_x[ i ];
+                data.magneticFieldY = mag_y[ i ];
+                data.magneticFieldZ = mag_z[ i ];
 
                 // 喂入校准器（累积数据）
-                calib.feed(data);
+                calib.feed( data );
             }
 
             SixParametersCorrector result = calib.computeResult();
             std::cout << "校准完成！" << std::endl;
 
             const std::string param_file = output_path;
-            if (result.toFile(param_file)) {
+            if ( result.toFile( param_file ) )
+            {
                 std::cout << "校准参数已保存到：" << param_file << std::endl;
-            } else {
+            }
+            else
+            {
                 std::cerr << "保存校准参数失败！" << std::endl;
                 return 1;
             }
