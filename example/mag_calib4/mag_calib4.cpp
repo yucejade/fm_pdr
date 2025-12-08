8362e3eee1bad859a23e954f5a4d9c02767fce56
yucejade <121438331@qq.com>
2025-12-08 15:31:00 +0800

    修改mag_calib程序

--- example/mag_calib4/mag_calib4.cpp
+++ example/mag_calib4/mag_calib4.cpp
@@ -89,21 +89,29 @@ int main( int argc, char* argv[] )
         return 0;
     }
 
-    bool type = parser.hasOption( "-t" ) || parser.hasOption( "--type" );
-
-    if ( type )  // 实时链接磁力计显示校准前后数值
+    int type;
+    bool has_type = parser.hasOption( "-t" ) || parser.hasOption( "--type" );
+    if ( ! has_type )
     {
-        std::string calibration_path = parser.getOption( "-c" );
-        if ( calibration_path.empty() )
-            calibration_path = parser.getOption( "--calibration-path" );
+        std::cerr << "参数错误!!!" << std::endl;
+        parser.showHelp();
+        return -1;
+    }
+    if ( parser.hasOption( "-t" ) )
+        type = parser.getIntOption( "-t" );
+    else if ( parser.hasOption( "--type" ) )
+        type = parser.getIntOption( "--type" );
+    else
+        type = 0;  // 默认类型
 
+    if ( type == 0 )  // 实时链接磁力计显示校准前后数值
+    {
         std::string mag_data_path = parser.getOption( "-d" );
-        if ( mag_data_path.empty() )
+        if ( mag_data_path.empty() ) {
             mag_data_path = parser.getOption( "--mag-data-path" );
-
-        // calibration_path和mag_data_path只允许输入一个
-        if ((calibration_path.empty() && mag_data_path.empty()) || (!calibration_path.empty() && !mag_data_path.empty()))
-            return -1;
+            if (mag_data_path.empty())
+                mag_data_path = "./mag_calib_data";
+        }
 
         int interval;
         if ( parser.hasOption( "-i" ) )
@@ -121,22 +129,6 @@ int main( int argc, char* argv[] )
             int                    count    = 0;
             SixParametersCorrector loaded_corrector;
             int                    ret = PDR_RESULT_SUCCESS;
-
-            if ( ! calibration_path.empty() )
-            {
-                if ( loaded_corrector.fromFile( calibration_path ) )
-                {
-                    std::cout << "成功加载校准参数！" << std::endl;
-                    // 打印加载的参数（可选，用于验证）
-                    std::cout << "加载的硬铁偏移（b）: " << loaded_corrector.getb().transpose() << " μT" << std::endl;
-                    std::cout << "加载的软铁增益（A）: " << loaded_corrector.getA().transpose() << "（无单位）" << std::endl;
-                }
-                else
-                {
-                    std::cerr << "加载校准参数失败！" << std::endl;
-                    return 1;
-                }
-            }
 
             ret = fm_device_init( 50, &device_handler );
             if ( ret != 0 )
@@ -161,40 +153,16 @@ int main( int argc, char* argv[] )
                 // 标记不是第一次读取数据，即不需要再次创建缓存
                 is_first = false;
 
-                if ( ! calibration_path.empty() )
+                // 转换为PDRData结构
+                PDRData pdr_data;
+                memset( &pdr_data, 0x00, sizeof( pdr_data ) );
+                pdr_data.sensor_data = sensor_data.sensor_data;
+
+                int result = fm_pdr_save_pdr_data( ( char* )mag_data_path.c_str(), &pdr_data );
+                if ( result != 0 )
                 {
-                    for ( int i = 0; i < length; ++i )
-                    {
-                        const double&    timestamp = sensor_data.sensor_data.acc_time[ i ];
-                        const double&    mag_x     = sensor_data.sensor_data.mag_x[ i ];
-                        const double&    mag_y     = sensor_data.sensor_data.mag_y[ i ];
-                        const double&    mag_z     = sensor_data.sensor_data.mag_z[ i ];
-                        MagnetometerData raw_data( timestamp, mag_x, mag_y, mag_z );
-                        Vector3f         raw_vec( raw_data.magneticFieldX, raw_data.magneticFieldY, raw_data.magneticFieldZ );
-
-                        // 调用校正方法（公式：校正后 = (原始数据 - 偏移) × 增益）
-                        Vector3f corrected_vec = loaded_corrector.correct( raw_vec );
-
-                        // 输出校正结果
-                        std::cout << "\n=== 数据校正示例 ===" << std::endl;
-                        std::cout << "原始数据: " << raw_vec.transpose() << " μT" << std::endl;
-                        std::cout << "校正后数据: " << corrected_vec.transpose() << ", " << corrected_vec.norm() << " μT" << std::endl;
-                    }
-                }
-
-                if ( ! mag_data_path.empty() )
-                {
-                    // 转换为PDRData结构
-                    PDRData pdr_data;
-                    memset( &pdr_data, 0x00, sizeof( pdr_data ) );
-                    pdr_data.sensor_data = sensor_data.sensor_data;
-
-                    int result = fm_pdr_save_pdr_data( ( char* )mag_data_path.c_str(), &pdr_data );
-                    if ( result != 0 )
-                    {
-                        std::cerr << "Failed to save data." << std::endl;
-                        continue;
-                    }
+                    std::cerr << "Failed to save data." << std::endl;
+                    continue;
                 }
             }
 
@@ -207,14 +175,14 @@ int main( int argc, char* argv[] )
             std::cerr << "磁力计校准失败" << std::endl;
         }
     }
-    else  // 输入磁力计数据生成校准文件
+    else if ( type == 1 ) // 输入磁力计数据生成校准文件
     {
         std::string mag_data_path = parser.getOption( "-d" );
         if ( mag_data_path.empty() )
         {
             mag_data_path = parser.getOption( "--mag-data-path" );
             if ( mag_data_path.empty() )
-                mag_data_path = "./Magnetometer.csv";
+                mag_data_path = "./mag_calib_data/Magnetometer.csv";
         }
 
         std::string output_path = parser.getOption( "-o" );
@@ -264,6 +232,97 @@ int main( int argc, char* argv[] )
         {
             std::cerr << "磁力计校准失败" << std::endl;
         }
+    }
+    else if (type == 2)
+    {
+        std::string calibration_path = parser.getOption( "-c" );
+        if ( calibration_path.empty() )
+        {
+            calibration_path = parser.getOption( "--calibration-path" );
+            if (calibration_path.empty())
+                calibration_path = "./mag_calib.csv";
+        }
+
+        int interval;
+        if ( parser.hasOption( "-i" ) )
+            interval = parser.getIntOption( "-i" );
+        else if ( parser.hasOption( "--interval" ) )
+            interval = parser.getIntOption( "--interval" );
+        else
+            interval = 180;  // 默认时间间隔
+
+        try
+        {
+            fm_device_handle_t     device_handler;
+            SensorData             sensor_data;
+            bool                   is_first = true;
+            int                    count    = 0;
+            SixParametersCorrector loaded_corrector;
+            int                    ret = PDR_RESULT_SUCCESS;
+
+            if ( loaded_corrector.fromFile( calibration_path ) )
+            {
+                std::cout << "成功加载校准参数！" << std::endl;
+                // 打印加载的参数（可选，用于验证）
+                std::cout << "加载的硬铁偏移（b）: " << loaded_corrector.getb().transpose() << " μT" << std::endl;
+                std::cout << "加载的软铁增益（A）: " << loaded_corrector.getA().transpose() << "（无单位）" << std::endl;
+            }
+            else
+            {
+                std::cerr << "加载校准参数失败！" << std::endl;
+                return 1;
+            }
+
+            ret = fm_device_init( 50, &device_handler );
+            if ( ret != 0 )
+                return PDR_RESULT_DEVICE_INIT_ERROR;
+
+            memset( &sensor_data, 0x00, sizeof( sensor_data ) );
+
+            if ( interval > 0 )
+                interval *= 50;
+            while ( interval <= 0 || ++count < interval )
+            {
+                // 使用固定缓存模式读取传感器数据
+                ret = fm_device_read( device_handler, is_first, 1, 1, &sensor_data );
+                if ( ret != 0 )
+                {
+                    std::cerr << "Sensor data reading failed." << std::endl;
+                    continue;
+                }
+
+                // 标记不是第一次读取数据，即不需要再次创建缓存
+                is_first = false;
+
+                const double&    timestamp = sensor_data.sensor_data.acc_time[ 0 ];
+                const double&    mag_x     = sensor_data.sensor_data.mag_x[ 0 ];
+                const double&    mag_y     = sensor_data.sensor_data.mag_y[ 0 ];
+                const double&    mag_z     = sensor_data.sensor_data.mag_z[ 0 ];
+                MagnetometerData raw_data( timestamp, mag_x, mag_y, mag_z );
+                Vector3f         raw_vec( raw_data.magneticFieldX, raw_data.magneticFieldY, raw_data.magneticFieldZ );
+
+                // 调用校正方法（公式：校正后 = (原始数据 - 偏移) × 增益）
+                Vector3f corrected_vec = loaded_corrector.correct( raw_vec );
+
+                // 输出校正结果
+                std::cout << "\n=== 数据校正示例 ===" << std::endl;
+                std::cout << "原始数据: " << raw_vec.transpose() << " μT" << std::endl;
+                std::cout << "校正后数据: " << corrected_vec.transpose() << ", " << corrected_vec.norm() << " μT" << std::endl;
+            }
+
+            // 释放设备读取缓存
+            fm_device_free_sensor_data( sensor_data );
+            fm_device_uninit( device_handler );
+        }
+        catch ( ... )
+        {
+            std::cerr << "磁力计校准失败" << std::endl;
+        }
+    }
+    else
+    {
+        std::cerr << "参数错误!!!" << std::endl;
+        parser.showHelp();
     }
     return 0;
 }
