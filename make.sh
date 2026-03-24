739b41afcf4462519de7d03ed5df451ed522dd61
yucejade <121438331@qq.com>
2026-03-24 17:47:43 +0800

    修改编译错误

--- make.sh
+++ make.sh
@@ -90,7 +90,7 @@ build_thirdparty() {
         cd ../.. || exit 1
 
         ### 编译fmm依赖库
-        # 编译readline库
+        # 编译ncurses库
         mkdir -p build/thirdparty/fmm//ncurses-6.6 && cd build/thirdparty/fmm//ncurses-6.6 || exit 1
         ../../../../thirdparty/fmm//ncurses-6.6/configure --prefix=$(pwd)/../../../package --build=${BUILD_TRIPLET}
         make install
@@ -156,26 +156,26 @@ build_thirdparty() {
                 exit 1
                 ;;
         esac
-        ../../../../thirdparty/fmm/openssl/Configure ${OPENSSL_TARGET} --prefix=${PACKAGE_PATH} --openssldir=${PACKAGE_PATH} shared zlib
+        ../../../../thirdparty/fmm/openssl/Configure ${OPENSSL_TARGET} --prefix=${PACKAGE_PATH} --openssldir=${PACKAGE_PATH} --libdir=lib shared zlib
         make install
         cd ../../../.. || exit 1
 
         # 编译curl库
         cd thirdparty/fmm/curl || exit 1
-        cmake -B ../../../build/thirdparty/fmm/curl -DCMAKE_INSTALL_PREFIX=../../../build/package -DCMAKE_PREFIX_PATH=../../../build/package -DCURL_USE_LIBPSL=OFF -DUSE_LIBIDN2=OFF -DUSE_NGHTTP2=OFF -S .
+        cmake -B ../../../build/thirdparty/fmm/curl -DCURL_USE_OPENSSL=ON -DCMAKE_INSTALL_PREFIX=../../../build/package -DCMAKE_PREFIX_PATH=../../../build/package -DCURL_USE_LIBPSL=OFF -DUSE_LIBIDN2=OFF -DUSE_NGHTTP2=OFF -DCURL_USE_OPENSSL=ON -S .
         make -C ../../../build/thirdparty/fmm/curl install
         cd ../../.. || exit 1
 
         # 编译gdal库
         cd thirdparty/fmm/gdal || exit 1
-        cmake -B ../../../build/thirdparty/fmm/gdal -DCMAKE_INSTALL_PREFIX=../../../build/package -DCMAKE_PREFIX_PATH=../../../build/package -DGDAL_ENABLE_DRIVER_GRIB=OFF -DGDAL_USE_ZSTD=OFF -DCMAKE_BUILD_TYPE=release -S .
+        cmake -B ../../../build/thirdparty/fmm/gdal -DCMAKE_INSTALL_PREFIX=../../../build/package -DCMAKE_PREFIX_PATH=../../../build/package -DGDAL_ENABLE_DRIVER_GRIB=OFF -DGDAL_USE_ZSTD=OFF -DPROJ_DIR=${PACKAGE_PATH}/lib/camke/proj -DCMAKE_BUILD_TYPE=release -S .
         make -C ../../../build/thirdparty/fmm/gdal install
         cd ../../.. || exit 1
 
         # 编译fmm库
-        cd thirdparty/fmm/fmm || exit 1
-        cmake -B ../../../build/thirdparty/fmm/fmm -DCMAKE_INSTALL_PREFIX=../../../build/package -DCMAKE_PREFIX_PATH=../../../build/package-DCMAKE_BUILD_TYPE=release -S .
-        make -C ../../../build/thirdparty/fmm/fmm install
+        cd thirdparty/fmm/fmm-master || exit 1
+        cmake -B ../../../build/thirdparty/fmm/fmm-master -DCMAKE_INSTALL_PREFIX=../../../build/package -DCMAKE_PREFIX_PATH=../../../build/package-DCMAKE_BUILD_TYPE=release -S .
+        make -C ../../../build/thirdparty/fmm/fmm-master install
         cd ../../.. || exit 1
 
         echo "Thirdparty build completed."
